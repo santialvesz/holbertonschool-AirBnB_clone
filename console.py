c87899b9f80e1cbc89f35834781fd2fb4bff130d
Santiago Alves <105250659+santialvesz@users.noreply.github.com>
2022-10-11 14:59:21 -0300

    fixes

--- console.py
+++ console.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 
-
 import cmd
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb)'
 
-def do_quit(self, line):
-    """Method to exit the HBNB console"""
-    exit()
+    def do_quit(self, args):
+        """Method to exit the HBNB console"""
+        exit()
 
-def do_EOF(self, line):
-    """"""
-    exit()
+    def do_EOF(self, line):
+        """"""
+        exit()
 
-def emptyline(self):
-    pass
+    def emptyline(self):
+        pass
+
+    def help(self):
+        print("Quit command to exit the program")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
