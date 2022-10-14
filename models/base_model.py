f59641f12278791276be0c0f8a9fb263a17fa939
santialvesz <santialvesdsc@gmail.com>
2022-10-13 23:59:22 -0300

    Adiing some descriptions and creating new files

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,25 @@
 import uuid
-from datetime import date, datetime
+from datetime import datetime
+import models
 
 class BaseModel():
+      """
+    Defines all common attributes/methods for other classes
+    """
     def __init__(self, *args, **kwargs):
+        """
+        This function is the constructor for the BaseModel class. It creates a new instance of the
+        BaseModel class and assigns it an id, created_at, and updated_at
+        """
         if len(kwargs) > 0:
             format = '%Y-%m-%dT%H:%M:%S.%f'
             for a in kwargs:
+                if a = "id":
+                    self.id = kwargs[a]
                 if a == "created_at":
-                    self.created_at = datetime.strptime(kwargs, format)
+                    self.created_at = datetime.strptime(kwargs[a], format)
                 if a == "updated_at":
-                    self.updated_at = datetime.strptime(kwargs, format)
+                    self.updated_at = datetime.strptime(kwargs[a], format)
 
         else:
             self.id = str(uuid.uuid4())
@@ -19,13 +29,22 @@ class BaseModel():
 
 
     def __str__(self):
-        return"[{}] ({}) {}".format(self.__class__.__name__ self.id, self.__dict__)
+        """
+        The function returns a string representation of the object
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """save doc"""
+        """"
+        The save function updates the updated_at attribute of the object to the current time and saves
+        the object to the JSON file.
+        """
         self.updated_at = datetime.now
-        
+        models.storage.save()
 
 
     def to_dict(self):
+         """
+        This function takes in an object and returns a dictionary representation of the object
+        """
         pass
