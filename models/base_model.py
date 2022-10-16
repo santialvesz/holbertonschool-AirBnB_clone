19f8805457f70f71f7c34ac5f7faf966e2704a10
santialvesz <santialvesdsc@gmail.com>
2022-10-16 20:41:28 -0300

    Fixing uuid

--- models/base_model.py
+++ models/base_model.py
@@ -30,6 +30,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
             
 
     def __str__(self):
