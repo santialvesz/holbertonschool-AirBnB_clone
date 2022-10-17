89ae653eca3f204dac9351631688dcfd10176939
santialvesz <santialvesdsc@gmail.com>
2022-10-17 20:44:49 -0300

    Re-making the funcs.

--- console.py
+++ console.py
@@ -34,45 +34,43 @@ class HBNBCommand(cmd.Cmd):
         """Function emptyline"""
         pass
 
-    def do_create(self, line):
-        if not line:
+def do_create(self, arg):
+        """Create a new instance"""
+        my_args = arg.split()
+        if len(my_args) == 0:
             print("** class name missing **")
             return
-
-        args = line.split()
-
-        if args[0] not in HBNBCommand._classes:
+        try:
+            new_ins = eval(my_args[0])()
+            new_ins.save()
+            print(new_ins.id)
+        except Exception:
             print("** class doesn't exist **")
-            return
 
-        new_obj = globals()[args[0]]()
-        new_obj.save()
-        print(new_obj.id)
-
-    def do_show(self, line):
-        if not line:
+def do_show(self, args):
+        """Prints the string representation of
+        an instance based on the class name and id"""
+        my_args = args.split()
+        if len(my_args) == 0:
             print("** class name missing **")
             return
-
-        args = line.split()
-
-        if args[0] not in HBNBCommand._classes:
-            print("** class doesn't exist **")
-            return
-
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
-
-        k = args[0] + "." + args[1]
-        instance = FileStorage()
-        new = models.storage.all()
-
-        for key, value in new.items():
-            if key == k:
-                print(value)
+        my_dict = storage.all()
+        for key, value in my_dict.items():
+            if my_args[0] == value.__class__.__name__:
+                if len(my_args) == 1:
+                    print("** instance id missing **")
+                    return
+                if my_args[1] == value.id:
+                    print(value)
+                    return
+        for k in dictionary.keys():
+            if my_args[0] == k:
+                print("** no instance found **")
                 return
-        print("** no instance found **")
+        print("** class doesn't exist **")
+
+        
+
 
 
 
