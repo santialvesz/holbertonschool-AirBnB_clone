303885eb5542e953108ce5d0bd6a49ad7b976c0f
santialvesz <santialvesdsc@gmail.com>
2022-10-16 21:01:30 -0300

    Fixing some identation problems

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-
-
+"""Model BaseModel doc"""
 import uuid
 from datetime import datetime
 import models
 
 
 class BaseModel():
-    """
-    Defines all common attributes/methods for other classes
-    """
+    """Defines all common attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
-         """
-        This function is the constructor for the BaseModel class. It creates a new instance of the
-        BaseModel class and assigns it an id, created_at, and updated_at
-        """
+        """This function is the constructor for the BaseModel class. It creates a new instance of the BaseModel class and assigns it an id, created_at, and updated_at"""
         if len(kwargs) > 0:
             fmt = '%Y-%m-%dT%H:%M:%S.%f'
             for i, j in kwargs.items():
@@ -31,20 +25,19 @@ class BaseModel():
             self.created_at = datetime.now()
             self.updated_at = self.created_at
             models.storage.new(self)
-            
 
     def __str__(self):
-         """
-        The function returns a string representation of the object
-        """
-         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """ The function returns a string representation of the object """
+        return "[{}] ({}) \
+    {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """The save function updates the updated_at attribute of the object to the current time and saves the object to the JSON file."""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """to dict doc"""
+        """This function takes in an object and returns a dictionary representation of the object"""
         dict_repr = self.__dict__.copy()
         dict_repr['created_at'] = self.created_at.isoformat()
         dict_repr['updated_at'] = self.updated_at.isoformat()
